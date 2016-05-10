3ac66357b3f60ff5440d5d64a1d834ba4338d282
Dave Gurnell <dave@untyped.com>
2016-05-10 12:34:54 +0100

    Added implementation of Color from beginning of ADTs
    This will be a follow-along-in-your-editor kind of thing.
    The exercise won't require developers to code anything
    unless they want to play with language features.
    
    The exercise code (when we create it) will consequently
    be the same as the solution code.

---
+++ src/test/scala/adt/color/ColorSpec.scala
@@ -0,0 +1,30 @@
+package adt.color
+
+import org.scalatest._
+
+class ColorSpec extends FlatSpec with Matchers {
+  "lightness" should "return the average of r, g, and b" in {
+    val color = Color(.1, .2, .3)
+    color.lightness should equal((color.r + color.g + color.b) / 3)
+  }
+
+  "lighterThan" should "identify that white is lighter than black" in {
+    val color1 = Color(1, 1, 1)
+    val color2 = Color(0, 0, 0)
+    (color1 lighterThan color2) should equal(true)
+    (color2 lighterThan color1) should equal(false)
+  }
+
+  it should "return false when both colors are the same" in {
+    val color = Color(.5, .5, .5)
+    (color lighterThan color) should equal(false)
+
+  }
+
+  "mostlyRed" should "classify primary colors correctly" in {
+    import Main.mostlyRed
+    mostlyRed(Color(1, 0, 0)) should equal(true)
+    mostlyRed(Color(0, 1, 0)) should equal(false)
+    mostlyRed(Color(0, 0, 1)) should equal(false)
+  }
+}
