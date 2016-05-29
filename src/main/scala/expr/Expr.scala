2ba91de385c3408b53c7b2eadbc988a10fb9fe5c
Dave Gurnell <dave@untyped.com>
2016-05-29 16:36:12 +0100

    Removed solutions to expressions exercise!

--- src/main/scala/expr/Expr.scala
+++ src/main/scala/expr/Expr.scala
@@ -1,20 +1,13 @@
 package expr
 
 object Expr {
-  def palindrome(str: String): String =
-    str + str.reverse.substring(1)
-
-  def greet(name: String): Unit =
-    println(s"Hello $name!")
-
-  def factorial(n: Int): Int =
-    if(n == 0 || n == 1) 1 else n * factorial(n - 1)
+  // TODO: Implement methods
 }
 
 object Main extends App {
   import Expr._
 
-  println("""palindrome("taco") == ${palindrome("taco")}""")
-  println("""greet("Earthlings") == ${greet("Earthlings")}""")
-  println("""factorial(10) == ${factorial(10)}""")
+  // println("""palindrome("taco") == ${palindrome("taco")}""")
+  // println("""greet("Earthlings") == ${greet("Earthlings")}""")
+  // println("""factorial(10) == ${factorial(10)}""")
 }
