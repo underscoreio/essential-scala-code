603e7e43dda64d48feb2f70df8186b757d55a13d
Renato Cavalcanti <renato@strongtyped.io>
2016-06-06 21:43:21 +0200

    palindrome test should be a boolean test, we should check if a string is a palindrome instead of comparing to itself

--- src/test/scala/expr/ExprSpec.scala
+++ src/test/scala/expr/ExprSpec.scala
@@ -6,11 +6,18 @@ class ExprSpec extends FlatSpec with Matchers {
   import Expr._
 
   "tacocat" should "be a palindrome" in {
-    palindrome("taco") should equal("tacocat")
+    pending
+    // palindrome("tacocat") should equal(true)
   }
 
-  "factorial" should "multiply some numbers and stuff" in {
-    factorial(3) should equal (3 * 2 * 1)
-    factorial(5) should equal (5 * 4 * 3 * 2 * 1)
+  "tacobell" should "NOT be a palindrome" in {
+    pending
+    // palindrome("tacobell") should equal(false)
+  }
+
+  "factorial" should "multiply some numbers and stuff" in {  
+    pending
+    // factorial(3) should equal (3 * 2 * 1)
+    // factorial(5) should equal (5 * 4 * 3 * 2 * 1)    
   }
 }
