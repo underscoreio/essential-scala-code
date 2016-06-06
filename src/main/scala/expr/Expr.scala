e0328146b050c1eb22e8c03e86c72639a3251894
Renato Cavalcanti <renato@strongtyped.io>
2016-06-06 22:07:03 +0200

    revert previous commit (student must add the method and enable test - aligning with the other exercises)

--- src/main/scala/expr/Expr.scala
+++ src/main/scala/expr/Expr.scala
@@ -3,11 +3,7 @@ package expr
 object Expr {
 
   // TODO: Implement methods
-  def palindrome(s: String): String = ???
 
-  def greet(s: String): String = ???
-
-  def factorial(num: Long): Long = ???
 }
 
 object Main extends App {
--- src/test/scala/expr/ExprSpec.scala
+++ src/test/scala/expr/ExprSpec.scala
@@ -7,13 +7,13 @@ class ExprSpec extends FlatSpec with Matchers {
 
   "tacocat" should "be a palindrome" in {
     pending
-    palindrome("tacocat") should equal("tacocat")
+    // palindrome("tacocat") should equal("tacocat")
   }
 
 
   "factorial" should "multiply some numbers and stuff" in {  
     pending
-    factorial(3) should equal (3 * 2 * 1)
-    factorial(5) should equal (5 * 4 * 3 * 2 * 1)    
+    // factorial(3) should equal (3 * 2 * 1)
+    // factorial(5) should equal (5 * 4 * 3 * 2 * 1)    
   }
 }
