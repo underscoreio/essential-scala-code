aa2675d40def641c36f6e8cfedef87150150b31a
Dave Gurnell <dave@untyped.com>
2016-12-14 10:30:09 +0000

    Tweaks to ADT calc exercise

--- src/main/scala/adt/calc/Calc.scala
+++ src/main/scala/adt/calc/Calc.scala
@@ -3,12 +3,17 @@ package adt.calc
 sealed trait Calc
 // TODO: Implement Calc
 
+object DoubleInterpreter {
+  // TODO: Implement a simple interpreter that uses Doubles
+}
+
 object Main extends App {
   // TODO:
-  // - Calculate and print: 1 + 2 * 3
-  // - Calculate and print: 1 * 2 + 3
+  // - Calculate and print: 1.1 + 2.2 * 3.3
+  // - Calculate and print: 1.1 * 2.2 + 3.3
 
   // TODO:
+  // - Implement squaring a number using Calc
   // - Implement pythagoras' theorem using Calc
   // - Use your pythagoras implementation to calculate
   //   the hypotenuse of a 3x4 triangle
