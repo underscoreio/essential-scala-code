fd1f29c4c912d1dc44c454ea2f599301020d78ee
Dave Gurnell <dave@untyped.com>
2016-12-14 07:03:37 -0500

    Fixes to list unit tests

--- src/test/scala/generic/mylist/MyListSpec.scala
+++ src/test/scala/generic/mylist/MyListSpec.scala
@@ -3,8 +3,8 @@ package generic.mylist
 import org.scalatest._
 
 class MyListSpec extends FlatSpec with Matchers {
-  val ints    = MyPair(1, MyPair(3, MyPair(5, MyNil)))
-  val strings = MyPair("foo", MyPair("bar", MyPair("baz", MyNil)))
+  val ints    = MyPair(1, MyPair(3, MyPair(5, MyNil())))
+  val strings = MyPair("foo", MyPair("bar", MyPair("baz", MyNil())))
 
   "myList.exists" should "work as expected" in {
     ints.exists(_ > 0) should equal(true)
@@ -15,37 +15,37 @@ class MyListSpec extends FlatSpec with Matchers {
 
   "myList.filter" should "work as expected" in {
     ints.filter(_ > 0) should equal(ints)
-    ints.filter(_ < 0) should equal(MyNil)
-    ints.filter(_ > 3) should equal(MyPair(5, MyNil))
+    ints.filter(_ < 0) should equal(MyNil())
+    ints.filter(_ > 3) should equal(MyPair(5, MyNil()))
   }
 
   "myList.find" should "work as expected" in {
     ints.find(_ > 0) should equal(MySome(1))
-    ints.find(_ < 0) should equal(MyNone)
+    ints.find(_ < 0) should equal(MyNone())
     ints.find(_ > 3) should equal(MySome(5))
   }
 
   "myList.map" should "work on integers" in {
     val actual   = ints.map(_ + 1)
-    val expected = MyPair(2, MyPair(4, MyPair(6, MyNil)))
+    val expected = MyPair(2, MyPair(4, MyPair(6, MyNil())))
     actual should equal(expected)
   }
 
   it should "work on strings" in {
     val actual   = strings.map(_ + "!")
-    val expected = MyPair("foo!", MyPair("bar!", MyPair("baz!", MyNil)))
+    val expected = MyPair("foo!", MyPair("bar!", MyPair("baz!", MyNil())))
     actual should equal(expected)
   }
 
   "myList.flatMap" should "work on integers" in {
-    val actual   = ints.flatMap(x => MyPair(x, MyPair(x * 10, MyNil)))
-    val expected = MyPair(1, MyPair(10, MyPair(3, MyPair(30, MyPair(5, MyPair(50, MyNil))))))
+    val actual   = ints.flatMap(x => MyPair(x, MyPair(x * 10, MyNil())))
+    val expected = MyPair(1, MyPair(10, MyPair(3, MyPair(30, MyPair(5, MyPair(50, MyNil()))))))
     actual should equal(expected)
   }
 
   it should "work on strings" in {
-    val actual   = strings.flatMap(x => MyPair(x, MyPair(x + "!", MyNil)))
-    val expected = MyPair("foo", MyPair("foo!", MyPair("bar", MyPair("bar!", MyPair("baz", MyPair("baz!", MyNil))))))
+    val actual   = strings.flatMap(x => MyPair(x, MyPair(x + "!", MyNil())))
+    val expected = MyPair("foo", MyPair("foo!", MyPair("bar", MyPair("bar!", MyPair("baz", MyPair("baz!", MyNil()))))))
     actual should equal(expected)
   }
 
@@ -66,8 +66,8 @@ class MyListSpec extends FlatSpec with Matchers {
   }
 
   it should "map over MyNone" in {
-    val actual   = (MyNone : MyOption[Int]).map(_ + 1)
-    val expected = MyNone
+    val actual   = MyNone[Int]().map(_ + 1)
+    val expected = MyNone()
     actual should equal(expected)
   }
 
@@ -82,10 +82,10 @@ class MyListSpec extends FlatSpec with Matchers {
 
   it should "flatMap over MyNone" in {
     val actual = for {
-      x <- (MyNone : MyOption[Int])
+      x <- MyNone[Int]()
       y <- MySome(32)
     } yield x + y
-    val expected = MyNone
+    val expected = MyNone()
     actual should equal(expected)
   }
 }
