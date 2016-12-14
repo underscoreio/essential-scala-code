96517ffdaf15851bfcedc1cf918c45dd3de76e42
Dave Gurnell <dave@untyped.com>
2016-12-14 10:04:42 +0000

    Add ADT calculator exercise

---
+++ src/main/scala/adt/calc/Calc.scala
@@ -0,0 +1,15 @@
+package adt.calc
+
+sealed trait Calc
+// TODO: Implement Calc
+
+object Main extends App {
+  // TODO:
+  // - Calculate and print: 1 + 2 * 3
+  // - Calculate and print: 1 * 2 + 3
+
+  // TODO:
+  // - Implement pythagoras' theorem using Calc
+  // - Use your pythagoras implementation to calculate
+  //   the hypotenuse of a 3x4 triangle
+}
