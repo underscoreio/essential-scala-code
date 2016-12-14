9b98e788f076eb07285db450eab12472c78d1c0c
Dave Gurnell <dave@untyped.com>
2016-12-14 05:42:45 -0500

    Add factorials to Calc exercise

--- src/main/scala/adt/calc/Calc.scala
+++ src/main/scala/adt/calc/Calc.scala
@@ -8,7 +8,8 @@ final case class Mul(a: Calc, b: Calc) extends Calc
 final case class Div(a: Calc, b: Calc) extends Calc
 final case class Sqrt(a: Calc) extends Calc
 
-object DoubleInterpreter {
+// TODO: Implement a simple interpreter for Calc
+object Interpreter {
   def run(calc: Calc): Double =
     calc match {
       case Num(a)    => a
@@ -20,6 +21,7 @@ object DoubleInterpreter {
     }
 }
 
+// TODO: Implement an interpreter that uses integer arithmetic
 object IntInterpreter {
   def run(calc: Calc): Int =
     calc match {
@@ -32,12 +34,15 @@ object IntInterpreter {
     }
 }
 
-sealed trait SafeResult
-final case class Passed(value: Double) extends SafeResult
-final case class Failed(message: String) extends SafeResult
-
+// TODO: Implement an interpreter that:
+// - reports division-by-zero
+// - reports square roots of negatives
 object SafeInterpreter {
-  def run(calc: Calc): SafeResult =
+  sealed trait Result
+  final case class Passed(value: Double) extends Result
+  final case class Failed(message: String) extends Result
+
+  def run(calc: Calc): Result =
     calc match {
       case Num(a) => Passed(a)
 
@@ -104,13 +109,13 @@ object Main extends App {
   // TODO:
   // - Calculate and print: 1.1 + 2.2 * 3.3
   val calc1 = Add(Num(1.1), Mul(Num(2.2), Num(3.3)))
-  println(DoubleInterpreter.run(calc1))
-  println(IntInterpreter.run(calc1))
+  println("calc1 " + Interpreter.run(calc1))
+  println("calc1 " + IntInterpreter.run(calc1))
 
   // - Calculate and print: 1.1 * 2.2 + 3.3
   val calc2 = Add(Mul(Num(1.1), Num(2.2)), Num(3.3))
-  println(DoubleInterpreter.run(calc2))
-  println(IntInterpreter.run(calc2))
+  println("calc2 " + Interpreter.run(calc2))
+  println("calc2 " + IntInterpreter.run(calc2))
 
   // TODO:
   // - Implement squaring a number:
@@ -123,11 +128,18 @@ object Main extends App {
 
   // - Use your pythagoras implementation to calculate
   //   the hypotenuse of a 3x4 triangle
-  println(DoubleInterpreter.run(pythag(3, 4)))
-  println(IntInterpreter.run(pythag(3, 4)))
+  println("pythag " + Interpreter.run(pythag(3, 4)))
+  println("pythag " + IntInterpreter.run(pythag(3, 4)))
+
+  // - Implement factorials using Calc:
+  def factorial(n: Int): Calc =
+    if(n < 1) Num(1) else Mul(Num(n), factorial(n - 1))
+
+  println("factorial " + Interpreter.run(factorial(10)))
+  println("factorial " + IntInterpreter.run(factorial(10)))
 
   // Demonstrate run fails gracefully:
-  println(SafeInterpreter.run(Sqrt(Num(-1))))
-  println(SafeInterpreter.run(Div(Num(1), Num(0))))
-  println(SafeInterpreter.run(pythag(-3, -4)))
+  println("safe interpreter " + SafeInterpreter.run(Sqrt(Num(-1))))
+  println("safe interpreter " + SafeInterpreter.run(Div(Num(1), Num(0))))
+  println("safe interpreter " + SafeInterpreter.run(pythag(-3, -4)))
 }
